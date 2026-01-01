c1b8f7f80314df8ad04c58fc2987bd44d395c5c1
koushik8686 <pinnukoushik1@gmail.com>
2026-01-01 17:35:49 +0530

    uip

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,8 @@ import { AppModule } from './app.module.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.init();
-  return app;
+  // app.setGlobalPrefix('api')
+  app.enableCors()
+  await app.listen(process.env.PORT ?? 3000);
 }
-
-export default bootstrap();
-
 bootstrap();
